900585faf794024cb927900b2a566bbc107890ac
BrRoman <editeur@traditions-monastiques.com>
2021-02-13 07:14:53 +0100

    List of books: first steps.

--- editor/apps/products/models.py
+++ editor/apps/products/models.py
@@ -1,3 +1,30 @@
+""" apps/products/models.py """
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """ Product model. """
+    ean = models.CharField(
+        max_length=13,
+        db_column='EAN',
+    )
+    ref_tm = models.CharField(
+        max_length=10,
+        db_column='Ref_TM',
+    )
+    title = models.CharField(
+        max_length=255,
+        db_column='Titre',
+    )
+    sub_title = models.CharField(
+        max_length=255,
+        db_column='Sous_titre',
+    )
+
+    class Meta:
+        managed = False
+        db_table = 'Objets'
+
+    def __str__(self):
+        return '{}: {}'.format(self.ref_tm, self.title)
--- editor/apps/products/urls.py
+++ editor/apps/products/urls.py
@@ -7,4 +7,5 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('books', views.books_list, name='books_list'),
 ]
