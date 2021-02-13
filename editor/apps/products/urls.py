98facb7640b1aae7f43720ec7da964a320671102
BrRoman <editeur@traditions-monastiques.com>
2021-02-13 11:05:47 +0100

    Urls improved/corr.

--- editor/apps/products/urls.py
+++ editor/apps/products/urls.py
@@ -1,6 +1,6 @@
 """ apps/products/urls.py """
 
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
--- editor/editor/urls.py
+++ editor/editor/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('editor/', include('apps.products.urls')),
+    path('admin', admin.site.urls),
+    path('editor', include('apps.products.urls')),
 ]
