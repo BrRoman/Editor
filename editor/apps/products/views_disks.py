c8b38566e5d0fe2d501bcd690302fb1a41584865
BrRoman <editeur@traditions-monastiques.com>
2021-03-23 12:32:32 +0100

    Disks: details OK.

--- editor/apps/products/views_disks.py
+++ editor/apps/products/views_disks.py
@@ -3,7 +3,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
-from .models import Product
+from .models import Charge, Product
 
 
 def disks_list(request):
@@ -28,11 +28,21 @@ def disk_create(request):
 def disk_details(request, **kwargs):
     """ Details of a disk. """
     disk = get_object_or_404(Product, pk=kwargs['pk'])
+    charges = Charge.objects.filter(product=disk)
+    total_charges = 0
+    for index, charge in enumerate(charges):
+        total_charges += charge.amount
+    cost = (total_charges / disk.circulation) if disk.circulation != 0 else 0
+    theorical_price = (cost * disk.coefficient) if disk.coefficient else 0
     return render(
         request,
         'products/disks/details.html',
         {
             'disk': disk,
+            'charges': charges,
+            'total_charges': total_charges,
+            'cost': '{:.2f}'.format(cost),
+            'theorical_price': '{:.2f}'.format(theorical_price),
         },
     )
 
